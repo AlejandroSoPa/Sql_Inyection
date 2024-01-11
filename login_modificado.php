6db8b22886e795a523620cb310be58f71582329e
AlejandroSoPa <asoladopalomino.cf@iesesteveterradas.cat>
2024-01-11 19:47:36 +0100

    Final commit

--- login_modificado.php
+++ login_modificado.php
@@ -3,38 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Sin Modificar</title>
+    <title>Login Modificado</title>
 </head>
 <body>
     <h1>LOGIN</h1>
     <form method="POST">
-        <input type="text" name="userName" placeholder="Usuari"></input>
-        <input type="password" name="pwd" placeholder="Contrasenya"></input>
+        <input type="text" name="userName" placeholder="Usuari">
+        <input type="password" name="pwd" placeholder="Contrasenya">
         <button type="submit">Enviar</button>
     </form>
 
     <?php
         if(isset($_POST['userName']) && isset($_POST['pwd'])){
             try {
-                $pwd=$_POST['pwd'];
+                $pwd = $_POST['pwd'];
                 $userName = $_POST['userName'];
                 $dsn = "mysql:host=localhost;dbname=mylogin";
                 $pdo = new PDO($dsn, 'super', '1q2w·E4r5t6y');
-
-                $query = $pdo->prepare("select nom from users where password = sha2(\"$pwd\",512) and nom= \"$userName\";");
+                
+                $query = $pdo->prepare("SELECT nom FROM users WHERE password = SHA2(:pwd, 512) AND nom = :userName");
+                $query->bindParam(':pwd', $pwd, PDO::PARAM_STR);
+                $query->bindParam(':userName', $userName, PDO::PARAM_STR);
                 $query->execute();
                 
                 $row = $query->fetch();
                 $correct = false;
-                while ( $row ) {
+                while ($row) {
                     echo "<h1>"."Bienvenido ". $row['nom']. "</h1>";
                     $row = $query->fetch();
-                    $correct=true;
+                    $correct = true;
                 }
-                if(!$correct){
+                if (!$correct) {
                     echo "Login incorrecto";
                 }
-                
+
             } catch (PDOException $e){
                 echo $e->getMessage();
             }
